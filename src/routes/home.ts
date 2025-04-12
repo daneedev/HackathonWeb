3cb710406560dea2bd3e80724f2307782c9af7ac
Danee <imdanee@seznam.cz>
2025-04-12 03:08:40 +0200

    soilStatus

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import fs from 'fs';
 const router = express.Router();
 
 router.get("/", function (req, res) {
+    // Read data from the file
+    const file = fs.readFileSync("./src/public/data/data.json", "utf-8");
+    const jsonData = JSON.parse(file);
+    const lastData = jsonData[jsonData.length - 1];
+    let soilStatus;
+    if (lastData.soilMoisture >= 280 && lastData.soilMoisture <= 380) {
+            soilStatus = 1;
+    } else if (lastData.soilMoisture > 380) {
+            soilStatus = 2;
+    } else if (lastData.soilMoisture < 280) {
+            soilStatus = 3;
+    }
     res.render("index.html", {
-
+        soilStatus: soilStatus,
     });
 });
 
